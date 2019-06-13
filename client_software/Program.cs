8267cfe5cc7e4a050a4af3549840a24d947d840f
Ryan.S <ryan2002.rs@gmail.com>
2019-06-13 13:04:54 +1000

    Chucked in CoreUI Server Template

---
+++ client_software/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using WindowsDisplayAPI;
+using WindowsDisplayAPI.DisplayConfig;
+using InTheHand.Graphics.Display;
+
+namespace WUAT {
+    internal class Program
+    {
+        Program()
+        {
+            Console.WriteLine("Hello World!");
+            foreach (var target in PathDisplayTarget.GetDisplayTargets())
+            {
+                Console.WriteLine("Path: "+target.DevicePath);
+                Console.WriteLine("Friendly Name: "+target.FriendlyName);
+                Console.WriteLine("Target ID: "+target.TargetId);
+                Console.WriteLine("Manufacture ID: "+target.EDIDManufactureId);
+                Console.WriteLine("Manufacture Code: "+target.EDIDManufactureCode);
+                Console.WriteLine("Product Code: "+target.EDIDProductCode);
+            }
+
+
+            Console.WriteLine("Display Amount: "+ PathDisplayTarget.GetDisplayTargets().Count());
+            Console.ReadLine();
+
+
+        }
+        public static void Main(string[] args)
+        {
+            new Program();
+        }
+        
+    }
+    
+}
