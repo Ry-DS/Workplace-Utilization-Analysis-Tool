6d01c9d38cd5fc85932619d4ed0a4a3e85e4efaa
Ryan.S <ryan2002.rs@gmail.com>
2019-06-17 09:35:22 +1000

    Added WUAT Logo

--- server_software/src/containers/DefaultLayout/DefaultLayout.js
+++ server_software/src/containers/DefaultLayout/DefaultLayout.js
@@ -1,10 +1,11 @@
-import React, { Component, Suspense } from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import React, {Component, Suspense} from 'react';
 import * as router from 'react-router-dom';
-import { Container } from 'reactstrap';
+import {Redirect, Route, Switch} from 'react-router-dom';
+import {Container} from 'reactstrap';
 
 import {
   AppAside,
+  AppBreadcrumb2 as AppBreadcrumb,
   AppFooter,
   AppHeader,
   AppSidebar,
@@ -12,24 +13,36 @@ import {
   AppSidebarForm,
   AppSidebarHeader,
   AppSidebarMinimizer,
-  AppBreadcrumb2 as AppBreadcrumb,
   AppSidebarNav2 as AppSidebarNav,
 } from '@coreui/react';
 // sidebar nav config
 import navigation from '../../_nav';
 // routes config
 import routes from '../../routes';
+//Logo for WUAT
+import wuatLogo from '../../assets/img/brand/wuat/WUAT Logo.svg';
+
+const logoStyle = {
+  width: '50px',
+  display: 'block',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  paddingTop: '10px',
+  paddingBottom: '10px'
+};
 
 const DefaultAside = React.lazy(() => import('./DefaultAside'));
 const DefaultFooter = React.lazy(() => import('./DefaultFooter'));
 const DefaultHeader = React.lazy(() => import('./DefaultHeader'));
 
-class DefaultLayout extends Component {
 
-  loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
+class DefaultLayout extends Component {
+  //WUAT LOGO STYLE
+
+  loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>;
 
   signOut(e) {
-    e.preventDefault()
+    e.preventDefault();
     this.props.history.push('/login')
   }
 
@@ -46,6 +59,8 @@ class DefaultLayout extends Component {
             <AppSidebarHeader />
             <AppSidebarForm />
             <Suspense>
+              {/*Sidebar*/}
+              <img src={wuatLogo} alt={wuatLogo} style={logoStyle}/>
             <AppSidebarNav navConfig={navigation} {...this.props} router={router}/>
             </Suspense>
             <AppSidebarFooter />
